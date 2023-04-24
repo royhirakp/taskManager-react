f721daee593fb13ff605deb7543b9d89ab082e3a
hirak <royhirakp@gmail.com>
2023-04-25 01:00:54 +0530

    commit new pagination

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,131 +1,161 @@
-import React from 'react'
-import axios from 'axios';
-import {  useEffect, useState } from "react";
+import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 // import { Navigate } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 // import Loader from '../Loader';
-import Loader from '../Loader/Loder';
+import Loader from "../Loader/Loder";
 // import Coockie from 'js-coocki'
 // import Loader from '../Card/Loder';
-import './Login.css'
+import "./Login.css";
 // import { useDispatch, useSelector } from '@reduxjs/toolkit';
-import { useDispatch, useSelector } from 'react-redux';
-import { setUserInfo } from '../../Store/slice/TaskListSlice';
+import { useDispatch, useSelector } from "react-redux";
+import { setUserInfo } from "../../Store/slice/TaskListSlice";
+// import Test from "../AddTaskForm/test";
 const Login = (props) => {
-  const [inputEmail, setInputaEmail] = useState('')
-  const [inputPassword, setInputaPassword] = useState('')
-  const navgate = useNavigate()
-  const [errorMessege, setErrorMessege] = useState('')
-  const [loader, setLoder] = useState(false)
-  const [appInformationStatus, setappInformationStatus ] = useState(false)
+  const [inputEmail, setInputaEmail] = useState("");
+  const [inputPassword, setInputaPassword] = useState("");
+  const navgate = useNavigate();
+  const [errorMessege, setErrorMessege] = useState("");
+  const [loader, setLoder] = useState(false);
+  const [appInformationStatus, setappInformationStatus] = useState(false);
   // const [checkboxStatus, setCheckboxStatus] = useState(null)
-  const [emailValidaion, setEmailValidation] = useState(false)
-  const [passWValidaion, setPassWValidation] = useState(false)
+  const [emailValidaion, setEmailValidation] = useState(false);
+  const [passWValidaion, setPassWValidation] = useState(false);
 
   const dispatch = useDispatch();
-  const data = useSelector(s=>s)
-  console.log(data)
+  const data = useSelector((s) => s);
+  console.log(data);
 
-  //email and password validation 
+  //email and password validation
   useEffect(() => {
-    if (/[0-9]/.test(inputPassword) && /[~!@#$%^&*]/.test(inputPassword)) setPassWValidation(false)
-    else if (inputPassword.length > 2) setPassWValidation(true)
-    else if (inputPassword.length === 0) setPassWValidation(false)
+    if (/[0-9]/.test(inputPassword) && /[~!@#$%^&*]/.test(inputPassword))
+      setPassWValidation(false);
+    else if (inputPassword.length > 2) setPassWValidation(true);
+    else if (inputPassword.length === 0) setPassWValidation(false);
 
-    if (/[@]/.test(inputEmail) && /[.]/.test(inputEmail)) setEmailValidation(false)
-    else if (inputEmail.length === 0) setEmailValidation(false)
-    else setEmailValidation(true)
-  }, [inputEmail, inputPassword])
+    if (/[@]/.test(inputEmail) && /[.]/.test(inputEmail))
+      setEmailValidation(false);
+    else if (inputEmail.length === 0) setEmailValidation(false);
+    else setEmailValidation(true);
+  }, [inputEmail, inputPassword]);
 
-  //Onsubmit function 
+  //Onsubmit function
   async function onsunmitFun(e) {
-    e.preventDefault()
-    setLoder(true)
+    e.preventDefault();
+    setLoder(true);
     try {
       const body = {
         email: inputEmail,
-        password: inputPassword
-      }
-    
-      let res = await axios.post('https://task-manager-gd9n.onrender.com/login', body)
-      dispatch(setUserInfo({email: inputEmail, token: res.data.token}))
-      setErrorMessege('')
-      navgate('./main')
- 
+        password: inputPassword,
+      };
+
+      let res = await axios.post(
+        "https://task-manager-gd9n.onrender.com/login",
+        body
+      );
+      dispatch(setUserInfo({ email: inputEmail, token: res.data.token }));
+      setErrorMessege("");
+      navgate("./main");
+
       // document.cookie = 'SEmail'+inputEmail+";path=http://localhost:3000"
       // document.cookie = 'Spassword'+inputPassword+";path=http://localhost:3000"
     } catch (error) {
-      setErrorMessege(error.response.data.status)
+      setErrorMessege(error.response.data.status);
     }
-    setLoder(false)
+    setLoder(false);
   }
 
   return (
-    <div className='loginBody'>
-      <div className='textForInformationApp' 
-      style={{visibility:`${appInformationStatus ? "hidden" : ""}`}}
-      
+    <div className="loginBody">
+      <div
+        className="textForInformationApp"
+        style={{ visibility: `${appInformationStatus ? "hidden" : ""}` }}
       >
-        <b>TASK MANAGER App</b> . 
-        
-        <p> UI web pages: login, singup, home, addTaskForm
-
-          <br /> try this email and password for quick access . 
-          <br /><b>  email: "12@12.com" ; password: "12@12.com" </b></p>
-         <p><u><i>Redux-toolkit,metarialUI, react, nodejs expressJs, mongoDb, 
-         Render.com(api), netlify.com(ui)</i></u></p>
+        <b>TASK MANAGER App</b> .
+        <p>
+          {" "}
+          UI web pages: login, singup, home, addTaskForm
+          <br /> try this email and password for quick access .
+          <br />
+          <b> email: "12@12.com" ; password: "12@12.com" </b>
+        </p>
+        <p>
+          <u>
+            <i>
+              Redux-toolkit,metarialUI, react, nodejs expressJs, mongoDb,
+              Render.com(api), netlify.com(ui)
+            </i>
+          </u>
+        </p>
       </div>
-      <div className='Login-container'
-      onMouseEnter={()=>{setappInformationStatus(true)}}
-      onMouseLeave={()=>{setappInformationStatus(false)}}
+      <div
+        className="Login-container"
+        onMouseEnter={() => {
+          setappInformationStatus(true);
+        }}
+        onMouseLeave={() => {
+          setappInformationStatus(false);
+        }}
       >
-
-        <p style={{ "textAlign": "center" }}><b>Sing In</b></p>
-        <form 
-        onSubmit={onsunmitFun}
-        >
+        <p style={{ textAlign: "center" }}>
+          <b>Sing In</b>
+        </p>
+        <form onSubmit={onsunmitFun}>
           <br />
           Email Address:
-          <input type='email'
-            placeholder='Enter email'
-            style={{ border: emailValidaion ? "red 4px solid" : '' }}
+          <input
+            type="email"
+            placeholder="Enter email"
+            style={{ border: emailValidaion ? "red 4px solid" : "" }}
             onChange={(e) => setInputaEmail(e.target.value)}
-          /><br/>
-          {emailValidaion ? <p style={{ color: "red" }}>not a valid email</p> : <></>}
+          />
+          <br />
+          {emailValidaion ? (
+            <p style={{ color: "red" }}>not a valid email</p>
+          ) : (
+            <></>
+          )}
           <br />
           Password
-          <input type='password'
-            placeholder='Enter password'
+          <input
+            type="password"
+            placeholder="Enter password"
             style={{ border: passWValidaion ? "red 4px solid" : "" }}
             onChange={(e) => setInputaPassword(e.target.value)}
-          /><br />
-
+          />
+          <br />
           <div className="chekboxRemberme">
-            <input type='checkbox'
-            // onClick={(e)=>{setCheckboxStatus(e.target.checked)}}
-            /> <p>Remember me? </p><br />
+            <input
+              type="checkbox"
+              // onClick={(e)=>{setCheckboxStatus(e.target.checked)}}
+            />{" "}
+            <p>Remember me?</p>
+            <br />
           </div>
-
-          {
-            passWValidaion ? <p style={{color:"red"}}>not a valid password</p>: <></>
-          }
-
+          {passWValidaion ? (
+            <p style={{ color: "red" }}>not a valid password</p>
+          ) : (
+            <></>
+          )}
           <button
-            className='submitButton'
-            style={{ "width": "100%" }}
-            type="submit">
+            className="submitButton"
+            style={{ width: "100%" }}
+            type="submit"
+          >
             Submit
           </button>
         </form>
-        Forgot <a href="http://ww.ckc.co">Password?</a><br />
-        <Link to="/register">SingUp</Link> 
-        {errorMessege ? <h3 style={{ color: 'red' }}>{errorMessege}</h3> : ""}
-        {loader ? <Loader/> : <></>}
-        
+        Forgot <a href="http://ww.ckc.co">Password?</a>
+        <br />
+        <Link to="/register">SingUp</Link>
+        {errorMessege ? <h3 style={{ color: "red" }}>{errorMessege}</h3> : ""}
+        {loader ? <Loader /> : <></>}
+        {/* <Test/> */}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Login;
--- src/components/TaskList/TaskListMain.jsx
+++ src/components/TaskList/TaskListMain.jsx
@@ -1,84 +1,129 @@
-import React, { useEffect, useState } from 'react'
-import './TaskListMain.css'
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
-import TaskCard from './TaskCard/TaskCard';
+import React, { useEffect, useState } from "react";
+import "./TaskListMain.css";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import TaskCard from "./TaskCard/TaskCard";
 import { useNavigate } from "react-router-dom";
-import axios from 'axios';
-import Loader from '../Loader/Loder';
-import { useRef } from 'react';
+import axios from "axios";
+import Loader from "../Loader/Loder";
+import { useRef } from "react";
+import Pagination from "@mui/material/Pagination";
+// {
+//   "explorer.confirmDelete": false,
+//   "git.suggestSmartCommit": false,
+//   "git.autofetch": true,
+//   "emmet.includeLanguages": {
+//       "javascript": "javascriptreact"
+//   },
+//   "explorer.confirmDragAndDrop": false,
+//   "workbench.colorTheme": "Monokai",
+//   "workbench.iconTheme": "eq-material-theme-icons"
+// }
 //redux
-import { useDispatch, useSelector } from 'react-redux';
-import { setTaskData } from '../../Store/slice/TaskListSlice';
-import { setUserInfo } from '../../Store/slice/TaskListSlice';
+import { useDispatch, useSelector } from "react-redux";
+import { setTaskData } from "../../Store/slice/TaskListSlice";
+import { setUserInfo } from "../../Store/slice/TaskListSlice";
+// import Pagination from "../Pagination/Pagination";
+// import Test from '../AddTaskForm/test';
 const TaskListMain = () => {
-  const [loader, setLoader] = useState(false)
-  const dispatch = useDispatch()
-  let Storedata = useSelector(s=>s)
-  let {deleteStatus} = Storedata.TaskManager
-  const tempFunction = useRef()
+  const [loader, setLoader] = useState(false);
+  const [pageData, setPageData] = useState([]);
+  const [totalPages, setTotalPages] = useState(0);
+  const dispatch = useDispatch();
+  let Storedata = useSelector((s) => s);
+  let { deleteStatus } = Storedata.TaskManager;
+  const tempFunction = useRef();
 
-  const navgate = useNavigate()
-  function handelAddTask(){
-    navgate('../addTask')
+  const navgate = useNavigate();
+  function handelAddTask() {
+    navgate("../addTask");
   }
 
- async function getData(){
-  if(Storedata.TaskManager.loginInfo.token === ""){
-    navgate('../')
-    return;
+  function pageDataSlice(arr, pageNo) {
+    let dataPerPage = 4;
+    let fstIndx = (pageNo - 1) * dataPerPage;
+    return arr.slice(fstIndx, fstIndx + dataPerPage);
   }
- 
+
+  async function getData() {
+    if (Storedata.TaskManager.loginInfo.token === "") {
+      navgate("../");
+      return;
+    }
+
     try {
-      setLoader(true)
+      setLoader(true);
       const config = {
         headers: {
-            authorization : Storedata.TaskManager.loginInfo.token                      
-        }
-    }
-    const res = await axios.get (`https://task-manager-gd9n.onrender.com/task`,config)
-    // console.log(res.data.data)
-    dispatch(setTaskData(res.data.data))
+          authorization: Storedata.TaskManager.loginInfo.token,
+        },
+      };
+      const res = await axios.get(
+        `https://task-manager-gd9n.onrender.com/task`,
+        config
+      );
+      // console.log(res.data.data);
+      //pagination
+      let data = [...res.data.data];
+      let lengthh = parseInt(data.length / 4);
+      if (lengthh !== data.length) lengthh = lengthh + 1;
+      setTotalPages(lengthh);
+      // redux
+      dispatch(setTaskData(res.data.data.reverse()));
+      let updateArr = pageDataSlice(data.reverse(), 1);
+      setPageData(updateArr);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-    setLoader(false)
+    setLoader(false);
   }
-  tempFunction.current = getData
+  tempFunction.current = getData;
 
-  useEffect(()=>{    
+  useEffect(() => {
     tempFunction.current();
-  },[deleteStatus])
+  }, [deleteStatus]);
+
   return (
     <div>
-        {loader ? <Loader/>:""}
-        <h1 style={{"textAlign":"center"}}>Task Manager</h1>
-        <nav>
-            <div className="UserInformation">
-                <p>Nmae:</p>
-                <p>User email: {Storedata.TaskManager.loginInfo.email}</p>
-            </div>
-            <Stack direction="row" spacing={5}>
-            <Button variant="contained" color="success"
-            onClick={handelAddTask}
-            >Add Task</Button>
-            <Button variant="contained" color="success"
-            onClick={()=>{
-              dispatch(setUserInfo({email: "", token: ""}))
-              navgate('../')
+      {loader ? <Loader /> : ""}
+      <h1 style={{ textAlign: "center" }}>Task Manager</h1>
+      <nav>
+        <div className="UserInformation">
+          <p>Nmae:</p>
+          <p>User email: {Storedata.TaskManager.loginInfo.email}</p>
+        </div>
+        {/* <Test/> */}
+        <Stack direction="row" spacing={5}>
+          <Button variant="contained" color="success" onClick={handelAddTask}>
+            Add Task
+          </Button>
+          <Button
+            variant="contained"
+            color="success"
+            onClick={() => {
+              dispatch(setUserInfo({ email: "", token: "" }));
+              navgate("../");
             }}
-            >logout</Button>
-            </Stack>
-        </nav>
-        <main className='mainLIST'>
-          {Storedata.TaskManager.Tasks?.map((item,indx)=>{
-            return (
-              <TaskCard data={item} key={indx*55}/>
-            )
-          })}
-        </main>
+          >
+            logout
+          </Button>
+        </Stack>
+      </nav>
+      <main className="mainLIST">
+        {pageData?.map((item, indx) => {
+          return <TaskCard data={item} key={indx * 55} />;
+        })}
+      </main>
+      <Pagination
+        count={totalPages}
+        onChange={(e, v) => {
+          //pagination
+          let updateArr = pageDataSlice(Storedata.TaskManager.Tasks, v);
+          setPageData(updateArr);
+        }}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default TaskListMain
+export default TaskListMain;
